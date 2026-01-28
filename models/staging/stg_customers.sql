49921eea010da586ebd714fb23612a78f2a66836
Erling Nordtvedt <e.t.nordtvedt@proton.me>
2026-01-28 22:51:18 +0000

    It won't connect to snowflake properly, fixing

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,23 +1,7 @@
-with
-
-source as (
+with source as (
 
     select * from {{ source('jaffle_shop', 'customers') }}
 
-),
-
-renamed as (
-
-    select
-
-        ----------  ids
-        id as customer_id,
-
-        ---------- text
-        name as customer_name
-
-    from source
-
 )
 
-select * from renamed
+select * from source
