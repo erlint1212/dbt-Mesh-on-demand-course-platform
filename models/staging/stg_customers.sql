8dbefafb1f759a82ea4aa884ef60bdc6930cea8b
Erling Nordtvedt <e.t.nordtvedt@proton.me>
2026-01-29 08:59:52 +0000

    Trying

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,7 +1,13 @@
 with source as (
-
     select * from {{ source('jaffle_shop', 'customers') }}
+),
 
+renamed as (
+    select
+        -- Adjust these based on the headers you see in the result grid!
+        id,
+        name as customer_name
+    from source
 )
 
-select * from source
+select * from renamed
